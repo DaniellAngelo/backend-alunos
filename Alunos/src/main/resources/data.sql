755d2ddf82f50a092408bbfe96cfcc016f6839b1
DaniellAngelo <dcsggg5@gmail.com>
2025-04-14 20:02:14 -0300

    melhorias e correcoes

---
+++ Alunos/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO aluno (nome, matricula) VALUES
+                                        ('João Silva', '20230001'),
+                                        ('Maria Oliveira', '20230002'),
+                                        ('Carlos Souza', '20230003');
